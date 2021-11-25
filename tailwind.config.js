19441e2fbf8c63fb713dcd318458554f6008824b
CodeBlack29 <39704797+vegas29@users.noreply.github.com>
2021-11-25 16:53:44 -0500

    Componente boton de parametrizar

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        brand: "#0fa9e6",
+        primario: "#40C2D6",
+        secundario: "#04DBFC"
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+
+  ],
+}
