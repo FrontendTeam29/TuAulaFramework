b9c5e3b00fb1bcf58b89a47c2bcdd4e08c920daa
jorgeduque <ivanduque-2003@hotmail.com>
2021-12-02 16:05:06 -0500

    calificar falta agregar el grid del tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,13 @@ module.exports = {
         primary: "#40C2D6",
         secondary: "#04DBFC",
         tertiary: "#DFE8F5"
+      },
+      gridTemplateColumns: {
+        // Simple 16 column grid
+       'rate': '4fr 3fr 1fr 1fr',
+
+        // Complex site-specific column configuration
+       
       }
     },
   },
